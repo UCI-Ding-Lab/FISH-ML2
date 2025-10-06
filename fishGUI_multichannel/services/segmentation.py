2ce765d41e6a66e667358e8eb85b394208f5426f
margoglvz <msgalvez06@gmail.com>
2025-10-05 17:10:48 -0700

    initializing seg variables

--- fishGUI_multichannel/services/segmentation.py
+++ fishGUI_multichannel/services/segmentation.py
@@ -116,7 +116,9 @@ def run_basic_watershed(
             except Exception as e:
                 logger.error(f"SAM refine failed on {channel} box {bb}: {str(e)}")
         
+        seg_647, seg_488, seg_555, seg_594 = [], [], [], []
         seg_objs = [segment(gui, m) for m in channel_masks]
+        
         if channel == "647":
             seg_647 = seg_objs
         elif channel == "488":
